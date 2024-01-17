cda021b06d44683c187766aa2898dcb18f795724
rydnr <github@acm-sl.org>
2024-01-17 19:18:09 +0100

    Added vim and emacs settings

--- pythoneda/sandbox/flows/sample/events/event1.py
+++ pythoneda/sandbox/flows/sample/events/event1.py
@@ -40,3 +40,11 @@ class Event1(Event):
         Creates a new Event1 instance.
         """
         super().__init__()
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
